154f23c5461d791eb69c6eb2b76605c0c8af29f2
Matthias Roth <matthias.roth@gesis.org>
2022-11-09 20:32:13 +0100

    added popup (modal) dialog with quantity question. also commit for transfer to pi.

--- shinyapp.R
+++ shinyapp.R
@@ -1,19 +1,20 @@
 library(shiny)
 library(DBI)
+library(shinythemes)
 
 db <- DBI::dbConnect(RSQLite::SQLite(),"poobase.sqlite")
 
-log_value <- function(event,db,table){
+log_value <- function(event,intensity,db,table){
   current_table <- dbReadTable(db,table)
   cur_time <- format(Sys.time(), "%Y-%m-%d %H:%M:%S")
   if(nrow(current_table) == 0) cur_id = 1 else cur_id = max(current_table$id)+1
-  next_values <- tibble::tibble(id = cur_id,time = cur_time,event = event)
+  next_values <- tibble::tibble(id = cur_id,time = cur_time,event = event,intensity = intensity)
   dbWriteTable(db,table,next_values,append = T)
 }
 
 create_table <- function(name,db){
   stopifnot(is.character(name))
-  table_to_write <- tibble::tibble(id = integer(),time = character(),event = character())
+  table_to_write <- tibble::tibble(id = integer(),time = character(),event = character(),intensity = integer())
   dbWriteTable(conn = db,name = name, table_to_write)
 }
 
@@ -31,32 +32,78 @@ reset_table <- function(name,db){
 if(!("pootable" %in% dbListTables(db))) create_table("pootable",db)
 
 ui <- fluidPage(
-  actionButton("log_poo","poo"),
-  actionButton("log_pee","pee"),
-  actionButton("log_scream","scream"),
-  actionButton("delete_last","delete last log"),
-  tableOutput("last_poo")
+  theme = shinytheme("united"),
+  titlePanel("PooTracker"),
+  tabsetPanel(
+    tabPanel(
+      "Data tracking",
+      fluidRow(
+        title = "Select ",
+        actionButton("log_poo","poo"),
+        actionButton("log_pee","pee"),
+        actionButton("log_scream","scream"),
+        actionButton("delete_last","delete last log")),
+      fluidRow(
+        tableOutput("last_poo"))
+    ),
+    tabPanel(
+      "Data analysis"
+    )
+  ),
+
 )
 
+modal_factory <- function(b_name,min_name,max_name,cur_rad){
+  function(){
+    cur_rad(b_name)
+    modalDialog(
+      title = "How much?",
+      radioButtons("rb","Select one:",
+                   setNames(c(1:5),
+                            c(min_name," "," "," ",max_name)),
+                   selected = character(0)),
+      easyClose = T,
+      size = "s",
+      footer = tagList(
+        actionButton("submit","Submit")))
+  }
+}
+
 server <- function(input, output, session) {
+  cur_rad <- reactiveVal()
+  poo_modal <- modal_factory("log_poo","Very litte","A lot",cur_rad)
+  pee_modal <- modal_factory("log_pee","Very litte","A lot",cur_rad)
+  scream_modal <- modal_factory("log_scream","Very litte","A lot",cur_rad)
 
   observeEvent(input$log_poo,
-               {log_value(event = "poo",db = db,table = "pootable")})
+               {showModal(poo_modal())})
 
   observeEvent(input$log_pee,
-               {log_value(event = "pee",db = db,table = "pootable")})
+               {showModal(pee_modal())})
 
   observeEvent(input$log_scream,
-               {log_value(event = "scream",db = db,table = "pootable")})
+               {showModal(scream_modal())})
 
   observeEvent(input$delete_last,
                {DBI::dbExecute(conn = db,paste0("DELETE FROM pootable WHERE ID=",nrow(retrive_table("pootable",db))))})
+
+  observeEvent(input$submit,
+               {which_modal <-dplyr::case_when(
+                  grepl("poo",cur_rad()) == T ~ "poo",
+                  grepl("pee",cur_rad()) == T ~ "pee",
+                  grepl("scream",cur_rad()) == T ~ "scream")
+               log_value(event = which_modal,
+                         intensity = input$rb,
+                         db = db,
+                         table = "pootable")
+                removeModal()
+               })
 
   lpt<-reactive({
     cur_table<-retrive_table("pootable",db)
     nrow_cur_table <-nrow(cur_table)
     tail(cur_table,n=pmin(5,nrow_cur_table))[rev(1:pmin(5,nrow_cur_table)),]
-    })|> bindEvent(input$log_poo,input$log_pee,input$log_scream,input$delete_last)
+    })|> bindEvent(input$submit,input$delete_last,ignoreNULL = T)
   output$last_poo <- renderTable(lpt())
 }
 
