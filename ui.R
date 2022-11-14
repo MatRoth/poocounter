3c884100d55ac2966b130fc0c3648cd9f3f02dd1
Matthias Roth <hmr1511@gmail.com>
2022-11-14 04:51:30 +0100

    starting transfere of old server and ui behavior to new layout

--- ui.R
+++ ui.R
@@ -1,30 +1,55 @@
 library(shiny)
-library(DBI)
-library(shinythemes)
-library(RSQLite)
-library(tibble)
-library(DBI)
-library(dplyr)
+library(shinyMobile)
+library(apexcharter)
+library(shinyWidgets)
 
-ui <- fluidPage(
-  theme = shinytheme("united"),
-  titlePanel("PooTracker"),
-  tabsetPanel(
-    tabPanel(
-      "Data tracking",
-      fluidRow(
-        title = "Select ",
-        actionButton("log_feed","Feeding"),
-        actionButton("log_poo","Poo"),
-        actionButton("log_pee","Pee"),
-        actionButton("log_scream","Scream"),
-        actionButton("delete_last","delete last log")),
-      fluidRow(
-        tableOutput("last_poo"))
+ui <- f7Page(
+  title = "Still tracker",
+  f7TabLayout(
+    panels = tagList(
+      f7Panel(title = "Anleitung", side = "left", theme = "light", "Im linken Tab kannst du Daten eintragen.\n Im rechten Tab kannst du dir Ergebnisse ansehen.", effect = "cover"),
+      f7Panel(title = "Right Panel", side = "right", theme = "dark", "Blabla", effect = "cover")
     ),
-    tabPanel(
-      "Data analysis"
-
+    options = list(theme = "md"),
+    navbar = f7Navbar(
+      title = "Still tracker",
+      hairline = TRUE,
+      shadow = TRUE,
+      leftPanel = TRUE,
+      rightPanel = F
+    ),
+    f7Tabs(
+      animated = TRUE,
+      #swipeable = TRUE,
+      f7Tab(
+        tabName = "Daten eintragen",
+        icon = f7Icon("pencile"),
+        active = TRUE,
+        f7Shadow(
+          intensity = 10,
+          hover = TRUE,
+          f7Card(
+            title = "Was willst du eintragen?",
+            f7List(
+              f7ListItem("Stillen",f7Button(inputId = "log_feed",label="Eintragen",size = "small")),
+              f7ListItem("Stuhlgang",f7Button(inputId = "log_food",label="Eintragen",size = "small")),
+              f7ListItem("Letzen Eintrag löschen",f7Button(inputId = "delete_last",label = "Löschen",size = "small"))
+            )
+          )
+        )
+      ),
+      f7Tab(
+        tabName = "Analyse",
+        icon = f7Icon("graph_square"),
+        active = FALSE,
+        f7Shadow(
+          intensity = 10,
+          hover = TRUE,
+          f7Card(
+            title = "Letztes Stillen",
+          )
+        )
+      )
     )
-  ),
+  )
 )
